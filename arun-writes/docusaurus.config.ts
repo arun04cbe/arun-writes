a685cab58bb608d0bf3591395d89ed38c0055f76
Arun N <arunnatarajan009@gmail.com>
2026-02-17 08:27:34 +0530

    add coding agents practical guide

--- arun-writes/docusaurus.config.ts
+++ arun-writes/docusaurus.config.ts
@@ -83,19 +83,19 @@ const config: Config = {
         src: "img/logo.svg",
       },
       items: [
-        {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
-          label: "Tutorial",
-        },
+        // {
+        //   type: "docSidebar",
+        //   sidebarId: "tutorialSidebar",
+        //   position: "left",
+        //   label: "Tutorial",
+        // },
         {
           type: "docSidebar",
           sidebarId: "genAISidebar",
           position: "left",
           label: "Gen AI",
         },
-        { to: "/blog", label: "Blog", position: "left" },
+        // { to: "/blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/arun04cbe",
           label: "GitHub",
